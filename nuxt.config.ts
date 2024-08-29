e98fd14cfc40165bfe2b67cf14b2860fa9f09847
sunny96087 <sunny96087@yahoo.com.tw>
2024-08-29 15:38:26 +0800

    feat: 修改網站名稱 & icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,11 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss', '@nuxt/icon', '@pinia/nuxt'],
-  css: ['~/assets/css/main.css']
+  css: ['~/assets/css/main.css'],
+  app: {
+    head: {
+      title: '鸚鸚食堂 Parrot Bistro', // 網站名稱
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/icon.png' }] // 網站 icon
+    }
+  }
 })
